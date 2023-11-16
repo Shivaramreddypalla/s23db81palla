eb2b4e6cb0b09b7f0c728abe42f7b6ff975a26db
Shivaram Reddy <shivaramreddy.91@gmail.com>
2023-11-15 20:15:16 -0600

    update an instance

--- controllers/Watch.js
+++ controllers/Watch.js
@@ -136,6 +136,38 @@ exports.Watch_create_Page = function (req, res) {
     }
 };
 
+// Handle building the view for updating a costume.
+// query provides the id
+exports.Watch_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Watch.findById(req.query.id)
+        res.render('Watchupdate', { title: 'Watch Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.Watch_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Watch.findById(req.query.id)
+        res.render('Watchdelete', {
+            title: 'Watch Delete', toShow:
+                result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
+
 
 
 
--- routes/Watch.js
+++ routes/Watch.js
@@ -5,8 +5,15 @@ var router = express.Router();
 router.get('/', Watch_controlers.Watch_view_all_Page );
 module.exports = router;
 
-/* GET detail costume page */
+/* GET detail watch page */
 router.get('/detail', Watch_controlers.Watch_view_one_Page);
 
-/* GET create costume page */
+/* GET create watch page */
 router.get('/create', Watch_controlers.Watch_create_Page);
+
+/* GET create update page */
+router.get('/update', Watch_controlers.Watch_update_Page);
+
+/* GET delete watch page */
+router.get('/delete', Watch_controlers.Watch_delete_Page);
+
